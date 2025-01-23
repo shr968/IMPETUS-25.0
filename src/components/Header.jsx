6fb84fc12830cbbba1319a030e2ca0349bf1b0a1
sanjaychethu <chethuhustler@gmail.com>
2025-01-23 18:02:56 +0530

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 text-3xl">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={ieee} width={90} height={35} alt="Brainwave" />
+          <img src={ieee} width={70} height={35} alt="Brainwave" />
         </a>
 
         <nav
